7e3c7bac5bf9221f562c8e7444518f70063b328a
Diogo <diogo.junior@grad.ufsc.br>
2018-06-18 16:17:49 -0300

    Add create tasks with POST

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,5 @@
 from todo import app, tasks
+import json
 
 def test_list_tasks_return_status_200():
     with app.test_client() as client:
@@ -29,4 +30,50 @@ def test_list_tasks_not_empty():
                                  b'"state":false,'
                                  b'"title":"tarefa 1"}]\n')
 
+def test_create_task_post():
+    with app.test_client() as client:
+        response = client.post('/task')
+        assert response.status_code != 405
 
+def test_create_task_return():
+    tasks.clear()
+    client = app.test_client()
+    response = client.post('/task', data=json.dumps({
+        'title': 'titulo',
+        'desc': 'descricao'
+        }),
+        content_type='application/json')
+    data = json.loads(response.data.decode('utf-8'))
+    assert data['id'] == 1
+    assert data['title'] == 'titulo'
+    assert data['desc'] == 'descricao'
+    assert data['state'] is False
+    assert response.status_code == 201
+
+def test_create_task_adds_to_database():
+    tasks.clear()
+    client = app.test_client()
+    # realiza a requisição utilizando o verbo POST
+    client.post('/task', data=json.dumps({
+        'title': 'titulo',
+        'desc': 'descricao'}),
+        content_type='application/json')
+    assert len(tasks) > 0
+
+def test_create_task_without_desc():
+    # status code should be 400 indicating client error
+    client = app.test_client()
+    response = client.post('/task', data=json.dumps({
+        'title': 'titulo'
+        }),
+        content_type='application/json')
+    assert response.status_code == 400
+
+def test_create_task_without_title():
+    # status code should be 400 indicating client error
+    client = app.test_client()
+    response = client.post('/task', data=json.dumps({
+        'desc': 'descricao'
+        }),
+        content_type='application/json')
+    assert response.status_code == 400
--- todo.py
+++ todo.py
@@ -1,14 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
 app = Flask('ToDo')
 tasks = []
 
 @app.route('/task')
 def list():
-    tasks.append({
-        'id': 1,
-        'title': 'tarefa 1',
-        'desc': 'primeira tarefa',
-        'state': False
-        })
     return jsonify(tasks)
+
+@app.route('/task', methods=['POST'])
+def create():
+    title = request.json.get('title')
+    desc = request.json.get('desc')
+    if not desc or not title:
+        abort(400)
+    task = {
+        'id':len(tasks)+1,
+        'title': title,
+        'desc': desc,
+        'state': False
+    }
+    tasks.append(task)
+    return jsonify(task), 201
