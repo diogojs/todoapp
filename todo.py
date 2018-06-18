56e310315372b4599b39c3b40a7c23afdf9be795
Diogo <diogo.junior@grad.ufsc.br>
2018-06-18 14:22:39 -0300

    Add Heroku integration

--- todo.py
+++ todo.py
@@ -5,4 +5,10 @@ tasks = []
 
 @app.route('/task')
 def list():
+    tasks.append({
+        'id': 1,
+        'title': 'tarefa 1',
+        'desc': 'primeira tarefa',
+        'state': False
+        })
     return jsonify(tasks)
